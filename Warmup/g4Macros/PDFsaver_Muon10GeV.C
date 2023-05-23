689c7dd3820395bdec3b0b1daabbc35212df1cbf
Carter Ryan Eikenbary <eikenbcr@login2.cluster>
2023-05-23 17:30:01 -0500

    Directory Changes

---
+++ Warmup/g4Macros/PDFsaver_Muon10GeV.C
@@ -0,0 +1,180 @@
+#define PDFsaver_Muon10GeV_cxx
+#include "PDFsaver_Muon10GeV.h"
+
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TF1.h>
+#include <TLine.h>
+#include <TH2.h>
+#include <TStyle.h>
+
+TH1D * Muon_PositionX = nullptr;
+TH1D * Muon_PositionY = nullptr;
+TH1D * Muon_PositionZ = nullptr;
+TH1D * Muon_MomentumX = nullptr;
+TH1D * Muon_MomentumY = nullptr;
+TH1D * Muon_MomentumZ = nullptr;
+TH1D * Muon_Energy = nullptr;
+
+TCanvas * c1 = nullptr;
+
+void PDFsaver_Muon10GeV::Begin(TTree * /*tree*/)
+{
+   TString option = GetOption();
+}
+
+void PDFsaver_Muon10GeV::SlaveBegin(TTree * /*tree*/)
+{
+   TString option = GetOption();
+
+Muon_PositionX = new TH1D("qpixg4 events", "Muon X Position", 50, 119, 121);
+Muon_PositionX->GetXaxis()->SetTitle("X Position (cm)");
+Muon_PositionX->GetYaxis()->SetTitle("Events / (0.2 cm)");
+Muon_PositionY = new TH1D("qpixg4 events", "Muon Y Position", 50, -1, 1);
+Muon_PositionY->GetXaxis()->SetTitle("Y Position (cm)");
+Muon_PositionY->GetYaxis()->SetTitle("Events / (0.2 cm)");
+Muon_PositionZ = new TH1D("qpixg4 events", "Muon Z Position", 125, 50, 300);
+Muon_PositionZ->GetXaxis()->SetTitle("Z Position (cm)");
+Muon_PositionZ->GetYaxis()->SetTitle("Events / (2.0 cm)");
+
+Muon_MomentumX = new TH1D("qpixg4 events", "Muon X Momentum", 50, -1, 1);
+Muon_MomentumX->GetXaxis()->SetTitle("X Momentum (MeV/c)");
+Muon_MomentumX->GetYaxis()->SetTitle("Events / (0.2 MeV/c)");
+Muon_MomentumY = new TH1D("qpixg4 events", "Muon Y Momentum", 50, 10105, 10107);
+Muon_MomentumY->GetXaxis()->SetTitle("Y Momentum (MeV/c)");
+Muon_MomentumY->GetYaxis()->SetTitle("Events / (0.2 MeV/c)");
+Muon_MomentumZ = new TH1D("qpixg4 events", "Muon Z Momentum", 50, -1, 1);
+Muon_MomentumZ->GetXaxis()->SetTitle("Z Momentum (MeV/c)");
+Muon_MomentumZ->GetYaxis()->SetTitle("Events / (0.2 MeV/c)");
+
+Muon_Energy = new TH1D("qpixg4 events", "Muon Energy", 50, 10105, 10107);
+Muon_Energy->GetXaxis()->SetTitle("Energy (MeV)");
+Muon_Energy->GetYaxis()->SetTitle("Events / (0.2 MeV)");
+
+c1 = new TCanvas("canvas1", "Test Canvas1");
+
+}
+
+Bool_t PDFsaver_Muon10GeV::Process(Long64_t entry)
+{
+
+  GetEntry(entry);
+  fReader.SetLocalEntry(entry);
+
+Muon_PositionX->Fill(generator_initial_particle_x[0]);
+Muon_PositionY->Fill(generator_initial_particle_y[0]);
+Muon_PositionZ->Fill(generator_initial_particle_z[0]);
+
+Muon_MomentumX->Fill(generator_initial_particle_px[0]);
+Muon_MomentumY->Fill(generator_initial_particle_py[0]);
+Muon_MomentumZ->Fill(generator_initial_particle_pz[0]);
+
+Muon_Energy->Fill(generator_initial_particle_energy[0]);
+
+   return kTRUE;
+}
+
+void PDFsaver_Muon10GeV::SlaveTerminate()
+{
+   // The SlaveTerminate() function is called after all entries or objects
+   // have been processed. When running with PROOF SlaveTerminate() is called
+   // on each slave server.
+
+}
+
+void PDFsaver_Muon10GeV::Terminate()
+{
+
+  c1->SetBottomMargin(0.2);
+  c1->SetLeftMargin(0.15);
+
+Muon_PositionX->GetXaxis()->CenterTitle(true);
+Muon_PositionX->GetXaxis()->SetTitleSize(20);
+Muon_PositionX->GetXaxis()->SetTitleFont(43);
+Muon_PositionX->GetXaxis()->SetTitleOffset(1.5);
+Muon_PositionX->GetXaxis()->SetLabelSize(0.05);
+Muon_PositionX->GetYaxis()->SetTitleSize(20);
+Muon_PositionX->GetYaxis()->SetTitleFont(43);
+Muon_PositionX->GetYaxis()->SetLabelSize(0.05);
+Muon_PositionX->Draw();
+c1->SaveAs("Muon_PositionX.pdf");
+c1->SaveAs("Muon_PositionX.png");
+   
+Muon_PositionY->GetXaxis()->CenterTitle(true);
+Muon_PositionY->GetXaxis()->SetTitleSize(20);
+Muon_PositionY->GetXaxis()->SetTitleFont(43);
+Muon_PositionY->GetXaxis()->SetTitleOffset(1.5);
+Muon_PositionY->GetXaxis()->SetLabelSize(0.05);
+Muon_PositionY->GetYaxis()->SetTitleSize(20);
+Muon_PositionY->GetYaxis()->SetTitleFont(43);
+Muon_PositionY->GetYaxis()->SetLabelSize(0.05);
+Muon_PositionY->Draw();
+c1->SaveAs("Muon_PositionY.pdf");
+c1->SaveAs("Muon_PositionY.png");
+   
+Muon_PositionZ->GetXaxis()->CenterTitle(true);
+Muon_PositionZ->GetXaxis()->SetTitleSize(20);
+Muon_PositionZ->GetXaxis()->SetTitleFont(43);
+Muon_PositionZ->GetXaxis()->SetTitleOffset(1.5);
+Muon_PositionZ->GetXaxis()->SetLabelSize(0.05);
+Muon_PositionZ->GetYaxis()->SetTitleSize(20);
+Muon_PositionZ->GetYaxis()->SetTitleFont(43);
+Muon_PositionZ->GetYaxis()->SetLabelSize(0.05);
+Muon_PositionZ->Draw();
+c1->SaveAs("Muon_PositionZ.pdf");
+c1->SaveAs("Muon_PositionZ.png");
+   
+Muon_MomentumX->GetXaxis()->CenterTitle(true);
+Muon_MomentumX->GetXaxis()->SetTitleSize(20);
+Muon_MomentumX->GetXaxis()->SetTitleFont(43);
+Muon_MomentumX->GetXaxis()->SetTitleOffset(1.5);
+Muon_MomentumX->GetXaxis()->SetLabelSize(0.05);
+Muon_MomentumX->GetYaxis()->SetTitleSize(20);
+Muon_MomentumX->GetYaxis()->SetTitleFont(43);
+Muon_MomentumX->GetYaxis()->SetLabelSize(0.05);
+Muon_MomentumX->Draw();
+c1->SaveAs("Muon_MomentumX.pdf");
+c1->SaveAs("Muon_MomentumX.png");
+   
+Muon_MomentumY->GetXaxis()->CenterTitle(true);
+Muon_MomentumY->GetXaxis()->SetTitleSize(20);
+Muon_MomentumY->GetXaxis()->SetTitleFont(43);
+Muon_MomentumY->GetXaxis()->SetTitleOffset(1.5);
+Muon_MomentumY->GetXaxis()->SetLabelSize(0.05);
+Muon_MomentumY->GetYaxis()->SetTitleSize(20);
+Muon_MomentumY->GetYaxis()->SetTitleFont(43);
+Muon_MomentumY->GetYaxis()->SetLabelSize(0.05);
+Muon_MomentumY->GetYaxis()->SetLabelSize(0.05);
+Muon_MomentumY->GetXaxis()->SetNdivisions(5);   
+Muon_MomentumY->Draw();
+c1->SaveAs("Muon_MomentumY.pdf");
+c1->SaveAs("Muon_MomentumY.png");
+   
+Muon_MomentumZ->GetXaxis()->CenterTitle(true);
+Muon_MomentumZ->GetXaxis()->SetTitleSize(20);
+Muon_MomentumZ->GetXaxis()->SetTitleFont(43);
+Muon_MomentumZ->GetXaxis()->SetTitleOffset(1.5);
+Muon_MomentumZ->GetXaxis()->SetLabelSize(0.05);
+Muon_MomentumZ->GetYaxis()->SetTitleSize(20);
+Muon_MomentumZ->GetYaxis()->SetTitleFont(43);
+Muon_MomentumZ->GetYaxis()->SetLabelSize(0.05);
+Muon_MomentumZ->Draw();
+c1->SaveAs("Muon_MomentumZ.pdf");
+c1->SaveAs("Muon_MomentumZ.png");
+   
+Muon_Energy->GetXaxis()->CenterTitle(true);
+Muon_Energy->GetXaxis()->SetTitleSize(20);
+Muon_Energy->GetXaxis()->SetTitleFont(43);
+Muon_Energy->GetXaxis()->SetTitleOffset(1.5);
+Muon_Energy->GetXaxis()->SetLabelSize(0.05);
+Muon_Energy->GetYaxis()->SetTitleSize(20);
+Muon_Energy->GetYaxis()->SetTitleFont(43);
+Muon_Energy->GetYaxis()->SetLabelSize(0.05);
+Muon_Energy->GetXaxis()->SetNdivisions(5);
+Muon_Energy->Draw();
+c1->SaveAs("Muon_Energy.pdf");
+c1->SaveAs("Muon_Energy.png");   
+}
---
+++ Warmup/rtdMacros/PixelMapping.h
@@ -0,0 +1,143 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Tue May 23 17:22:14 2023 by ROOT version 6.26/06
+// from TTree event_tree/event tree
+// found on file: /scratch/user/eikenbcr/DUNE/Warmup/muon_rtd.root
+//////////////////////////////////////////////////////////
+
+#ifndef PixelMapping_h
+#define PixelMapping_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TSelector.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
+
+// Headers needed by this particular selector
+#include <vector>
+
+
+
+class PixelMapping : public TSelector {
+public :
+   TTreeReader     fReader;  //!the tree reader
+   TTree          *fChain = 0;   //!pointer to the analyzed TTree or TChain
+
+   // Readers to access the data (delete the ones you do not need).
+   TTreeReaderValue<Int_t> run = {fReader, "run"};
+   TTreeReaderValue<Int_t> event = {fReader, "event"};
+   TTreeReaderValue<Int_t> generator_initial_number_particles = {fReader, "generator_initial_number_particles"};
+   TTreeReaderArray<double> generator_initial_particle_x = {fReader, "generator_initial_particle_x"};
+   TTreeReaderArray<double> generator_initial_particle_y = {fReader, "generator_initial_particle_y"};
+   TTreeReaderArray<double> generator_initial_particle_z = {fReader, "generator_initial_particle_z"};
+   TTreeReaderArray<double> generator_initial_particle_t = {fReader, "generator_initial_particle_t"};
+   TTreeReaderArray<double> generator_initial_particle_px = {fReader, "generator_initial_particle_px"};
+   TTreeReaderArray<double> generator_initial_particle_py = {fReader, "generator_initial_particle_py"};
+   TTreeReaderArray<double> generator_initial_particle_pz = {fReader, "generator_initial_particle_pz"};
+   TTreeReaderArray<double> generator_initial_particle_energy = {fReader, "generator_initial_particle_energy"};
+   TTreeReaderArray<int> generator_initial_particle_pdg_code = {fReader, "generator_initial_particle_pdg_code"};
+   TTreeReaderArray<double> generator_initial_particle_mass = {fReader, "generator_initial_particle_mass"};
+   TTreeReaderArray<double> generator_initial_particle_charge = {fReader, "generator_initial_particle_charge"};
+   TTreeReaderValue<Int_t> generator_final_number_particles = {fReader, "generator_final_number_particles"};
+   TTreeReaderArray<double> generator_final_particle_x = {fReader, "generator_final_particle_x"};
+   TTreeReaderArray<double> generator_final_particle_y = {fReader, "generator_final_particle_y"};
+   TTreeReaderArray<double> generator_final_particle_z = {fReader, "generator_final_particle_z"};
+   TTreeReaderArray<double> generator_final_particle_t = {fReader, "generator_final_particle_t"};
+   TTreeReaderArray<double> generator_final_particle_px = {fReader, "generator_final_particle_px"};
+   TTreeReaderArray<double> generator_final_particle_py = {fReader, "generator_final_particle_py"};
+   TTreeReaderArray<double> generator_final_particle_pz = {fReader, "generator_final_particle_pz"};
+   TTreeReaderArray<double> generator_final_particle_energy = {fReader, "generator_final_particle_energy"};
+   TTreeReaderArray<int> generator_final_particle_pdg_code = {fReader, "generator_final_particle_pdg_code"};
+   TTreeReaderArray<double> generator_final_particle_mass = {fReader, "generator_final_particle_mass"};
+   TTreeReaderArray<double> generator_final_particle_charge = {fReader, "generator_final_particle_charge"};
+   TTreeReaderValue<Int_t> number_particles = {fReader, "number_particles"};
+   TTreeReaderValue<Int_t> number_hits = {fReader, "number_hits"};
+   TTreeReaderValue<Double_t> energy_deposit = {fReader, "energy_deposit"};
+   TTreeReaderArray<int> particle_track_id = {fReader, "particle_track_id"};
+   TTreeReaderArray<int> particle_parent_track_id = {fReader, "particle_parent_track_id"};
+   TTreeReaderArray<int> particle_pdg_code = {fReader, "particle_pdg_code"};
+   TTreeReaderArray<double> particle_mass = {fReader, "particle_mass"};
+   TTreeReaderArray<double> particle_charge = {fReader, "particle_charge"};
+   TTreeReaderArray<int> particle_process_key = {fReader, "particle_process_key"};
+   TTreeReaderArray<int> particle_total_occupancy = {fReader, "particle_total_occupancy"};
+   TTreeReaderArray<double> particle_initial_x = {fReader, "particle_initial_x"};
+   TTreeReaderArray<double> particle_initial_y = {fReader, "particle_initial_y"};
+   TTreeReaderArray<double> particle_initial_z = {fReader, "particle_initial_z"};
+   TTreeReaderArray<double> particle_initial_t = {fReader, "particle_initial_t"};
+   TTreeReaderArray<double> particle_initial_px = {fReader, "particle_initial_px"};
+   TTreeReaderArray<double> particle_initial_py = {fReader, "particle_initial_py"};
+   TTreeReaderArray<double> particle_initial_pz = {fReader, "particle_initial_pz"};
+   TTreeReaderArray<double> particle_initial_energy = {fReader, "particle_initial_energy"};
+   TTreeReaderArray<int> particle_number_daughters = {fReader, "particle_number_daughters"};
+   TTreeReaderArray<vector<int>> particle_daughter_track_id = {fReader, "particle_daughter_track_id"};
+   TTreeReaderArray<int> hit_track_id = {fReader, "hit_track_id"};
+   TTreeReaderArray<double> hit_start_x = {fReader, "hit_start_x"};
+   TTreeReaderArray<double> hit_start_y = {fReader, "hit_start_y"};
+   TTreeReaderArray<double> hit_start_z = {fReader, "hit_start_z"};
+   TTreeReaderArray<double> hit_start_t = {fReader, "hit_start_t"};
+   TTreeReaderArray<double> hit_end_x = {fReader, "hit_end_x"};
+   TTreeReaderArray<double> hit_end_y = {fReader, "hit_end_y"};
+   TTreeReaderArray<double> hit_end_z = {fReader, "hit_end_z"};
+   TTreeReaderArray<double> hit_end_t = {fReader, "hit_end_t"};
+   TTreeReaderArray<double> hit_energy_deposit = {fReader, "hit_energy_deposit"};
+   TTreeReaderArray<double> hit_length = {fReader, "hit_length"};
+   TTreeReaderArray<int> hit_process_key = {fReader, "hit_process_key"};
+   TTreeReaderArray<int> pixel_x = {fReader, "pixel_x"};
+   TTreeReaderArray<int> pixel_y = {fReader, "pixel_y"};
+   TTreeReaderArray<vector<double>> pixel_reset = {fReader, "pixel_reset"};
+   TTreeReaderArray<vector<double>> pixel_tslr = {fReader, "pixel_tslr"};
+   TTreeReaderArray<vector<vector<int> >> pixel_reset_truth_track_id = {fReader, "pixel_reset_truth_track_id"};
+   TTreeReaderArray<vector<vector<int> >> pixel_reset_truth_weight = {fReader, "pixel_reset_truth_weight"};
+
+
+   PixelMapping(TTree * /*tree*/ =0) { }
+   virtual ~PixelMapping() { }
+   virtual Int_t   Version() const { return 2; }
+   virtual void    Begin(TTree *tree);
+   virtual void    SlaveBegin(TTree *tree);
+   virtual void    Init(TTree *tree);
+   virtual Bool_t  Notify();
+   virtual Bool_t  Process(Long64_t entry);
+   virtual Int_t   GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; }
+   virtual void    SetOption(const char *option) { fOption = option; }
+   virtual void    SetObject(TObject *obj) { fObject = obj; }
+   virtual void    SetInputList(TList *input) { fInput = input; }
+   virtual TList  *GetOutputList() const { return fOutput; }
+   virtual void    SlaveTerminate();
+   virtual void    Terminate();
+
+   ClassDef(PixelMapping,0);
+
+};
+
+#endif
+
+#ifdef PixelMapping_cxx
+void PixelMapping::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the reader is initialized.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   fReader.SetTree(tree);
+}
+
+Bool_t PixelMapping::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+
+#endif // #ifdef PixelMapping_cxx
