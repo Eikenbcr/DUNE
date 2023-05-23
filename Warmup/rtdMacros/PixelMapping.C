44aeb2bdcad430424c7ff6189aacd3d54ad59b73
Carter Eikenbary <eikenbcr@tamu.edu>
2023-05-23 18:28:23 -0500

    Update PixelMapping.C
    changes to graph

--- Warmup/rtdMacros/PixelMapping.C
+++ Warmup/rtdMacros/PixelMapping.C
@@ -49,9 +49,10 @@ void PixelMapping::SlaveTerminate()
 
 void PixelMapping::Terminate()
 {
-  gStyle->SetNumberContours(2);
+
   c1->SetBottomMargin(0.2);
   c1->SetLeftMargin(0.15);
+  c1->SetGrid();   
    
 Pixel_Mapping->GetXaxis()->CenterTitle(true);
 Pixel_Mapping->GetXaxis()->SetTitleSize(20);
@@ -61,6 +62,9 @@ Pixel_Mapping->GetXaxis()->SetLabelSize(0.05);
 Pixel_Mapping->GetYaxis()->SetTitleSize(20);
 Pixel_Mapping->GetYaxis()->SetTitleFont(43);
 Pixel_Mapping->GetYaxis()->SetLabelSize(0.05);
+Pixel_Mapping->GetXaxis()->SetNdivisions(125);
+Pixel_Mapping->GetYaxis()->SetNdivisions(125);
+Pixel_Mapping->GetZaxis()->SetNdivisions(2);   
 Pixel_Mapping->Draw("COLZ");
 c1->SaveAs("Pixel_Mapping.pdf");
 c1->SaveAs("Pixel_Mapping.png");
