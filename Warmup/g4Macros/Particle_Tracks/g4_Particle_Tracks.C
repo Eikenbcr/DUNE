54652a2ab549a1904be7d4ff5423a0955016336e
Carter Eikenbary <eikenbcr@tamu.edu>
2023-05-25 16:32:41 -0500

    Update g4_Particle_Tracks.C

--- Warmup/g4Macros/Particle_Tracks/g4_Particle_Tracks.C
+++ Warmup/g4Macros/Particle_Tracks/g4_Particle_Tracks.C
@@ -11,9 +11,6 @@
 #include <TH2.h>
 #include <TStyle.h>
 
-TH2D * Track_XY_Init = nullptr;
-TH2D * Track_XZ_Init = nullptr;
-
  vector <double> x_init(0);
  vector <double> x_fin(0);
  vector <double> y_init(0);
@@ -21,14 +18,10 @@ TH2D * Track_XZ_Init = nullptr;
  vector <double> z_init(0);
  vector <double> z_fin(0);
 
-
-TH2D * Track_YZ_Init = nullptr;
-TH3D * Track_XYZ_Init = nullptr;
-
-TH2D * Track_XY_Fin = nullptr;
-TH2D * Track_XZ_Fin = nullptr;
-TH2D * Track_YZ_Fin = nullptr;
-TH3D * Track_XYZ_Fin = nullptr;
+TH2D * Track_XY = nullptr;
+TH2D * Track_YZ = nullptr;
+TH2D * Track_XZ = nullptr;
+TH3D * Track_XYZ = nullptr;
 
 TCanvas * c1 = nullptr;
 
@@ -41,47 +34,23 @@ void g4_Particle_Tracks::SlaveBegin(TTree * /*tree*/)
 {
    TString option = GetOption();
    
-   Track_XY_Init = new TH2D("qpixg4 events", "Initial Track Hits", 575, 0, 230, 1500, 0, 600);
-   Track_XY_Init->GetXaxis()->SetTitle("X Position [cm]");
-   Track_XY_Init->GetYaxis()->SetTitle("Y Position [cm]");
-   Track_XY_Init->SetMarkerColor(kBlue);
+   Track_XY = new TH2D("qpixg4 events", "Track Hits in X-Y Plane", 575, 0, 230, 1500, 0, 600);
+   Track_XY->GetXaxis()->SetTitle("X Position [cm]");
+   Track_XY->GetYaxis()->SetTitle("Y Position [cm]");
    
-   Track_XY_Fin = new TH2D("qpixg4 events", "Final Track Hits", 575, 0, 230, 1500, 0, 600);
-   Track_XY_Fin->GetXaxis()->SetTitle("X Position [cm]]");
-   Track_XY_Fin->GetYaxis()->SetTitle("Y Position [cm]");
-   Track_XY_Fin->SetFillColor(kRed);      
+   Track_XZ = new TH2D("qpixg4 events", "Track Hits in X-Z Plane", 575, 0, 230, 900, 0, 360);
+   Track_XZ->GetXaxis()->SetTitle("X Position [cm]");
+   Track_XZ->GetYaxis()->SetTitle("Z Position [cm]");
    
-   Track_XZ_Init = new TH2D("qpixg4 events", "Initial Track Hits", 575, 0, 230, 900, 0, 360);
-   Track_XZ_Init->GetXaxis()->SetTitle("X Position [cm]");
-   Track_XZ_Init->GetYaxis()->SetTitle("Z Position [cm]");
-   Track_XZ_Init->SetMarkerColor(kBlue);
+   Track_YZ = new TH2D("qpixg4 events", "Track Hits in Y-Z Plane", 1500, 0, 600, 900, 0, 360);
+   Track_YZ->GetXaxis()->SetTitle("Y Position [cm]");
+   Track_YZ->GetYaxis()->SetTitle("Z Position [cm]");
    
-   Track_XZ_Fin = new TH2D("qpixg4 events", "Final Track Hits", 575, 0, 230, 900, 0, 360);
-   Track_XZ_Fin->GetXaxis()->SetTitle("X Position [cm]]");
-   Track_XZ_Fin->GetYaxis()->SetTitle("Z Position [cm]");
-   Track_XZ_Fin->SetMarkerColor(kRed);   
-   
-   Track_YZ_Init = new TH2D("qpixg4 events", "Initial Track Hits", 1500, 0, 600, 900, 0, 360);
-   Track_YZ_Init->GetXaxis()->SetTitle("Y Position [cm]");
-   Track_YZ_Init->GetYaxis()->SetTitle("Z Position [cm]");
-   Track_YZ_Init->SetMarkerColor(kBlue);
-   
-   Track_YZ_Fin = new TH2D("qpixg4 events", "Final Track Hits", 1500, 0, 600, 900, 0, 360);
-   Track_YZ_Fin->GetXaxis()->SetTitle("Y Position [cm]]");
-   Track_YZ_Fin->GetYaxis()->SetTitle("Z Position [cm]");
-   Track_YZ_Fin->SetMarkerColor(kRed);   
-   
-   Track_XYZ_Init = new TH3D("qpixg4 events", "Initial Track Hits", 575, 0, 230, 1500, 0, 600, 900, 0, 360);
-   Track_XYZ_Init->GetXaxis()->SetTitle("X Position [cm]");
-   Track_XYZ_Init->GetYaxis()->SetTitle("Y Position [cm]");
-   Track_XYZ_Init->GetZaxis()->SetTitle("Z Position [cm]");
-   Track_XYZ_Init->SetMarkerColor(kBlue);
-   
-   Track_XYZ_Fin = new TH3D("qpixg4 events", "Final Track Hits", 575, 0, 230, 1500, 0, 600, 900, 0, 360);
-   Track_XYZ_Fin->GetXaxis()->SetTitle("X Position [cm]]");
-   Track_XYZ_Fin->GetYaxis()->SetTitle("Y Position [cm]");
-   Track_XYZ_Fin->GetZaxis()->SetTitle("Z Position [cm]");
-   Track_XYZ_Fin->SetMarkerColor(kRed);   
+   Track_XYZ = new TH3D("qpixg4 events", "Track Hits in 3D", 575, 0, 230, 1500, 0, 600, 900, 0, 360);
+   Track_XYZ->GetXaxis()->SetTitle("X Position [cm]]");
+   Track_XYZ->GetYaxis()->SetTitle("Y Position [cm]");
+   Track_XYZ->GetZaxis()->SetTitle("Z Position [cm]");
+
    
    c1 = new TCanvas("canvas1", "Test Canvas1");      
 }
@@ -98,15 +67,6 @@ Bool_t g4_Particle_Tracks::Process(Long64_t entry)
    
    for (int i=0; i < hit_start_x.GetSize(); i++){
       if (Event){
-         
-//  Track_XY_Init->Fill(hit_start_x[i], hit_start_y[i]);   
-//  Track_XY_Fin->Fill(hit_end_x[i], hit_end_y[i]);
-  Track_XZ_Init->Fill(hit_start_x[i], hit_start_z[i]);   
-  Track_XZ_Fin->Fill(hit_end_x[i], hit_end_z[i]);       
-  Track_YZ_Init->Fill(hit_start_y[i], hit_start_z[i]);   
-  Track_YZ_Fin->Fill(hit_end_y[i], hit_end_z[i]);     
-  Track_XYZ_Init->Fill(hit_start_x[i], hit_start_y[i], hit_start_z[i]);   
-  Track_XYZ_Fin->Fill(hit_end_x[i], hit_end_y[i], hit_end_z[i]);   
 
   x_init.push_back(hit_start_x[i]);        
   x_fin.push_back(hit_end_x[i]);  
@@ -131,69 +91,84 @@ void g4_Particle_Tracks::Terminate()
   c1->SetLeftMargin(0.15);
   gStyle->SetOptStat(0);   
    
- Track_XY_Init->GetXaxis()->CenterTitle(true);
- Track_XY_Init->GetXaxis()->SetTitleSize(20);
- Track_XY_Init->GetXaxis()->SetTitleFont(43);
- Track_XY_Init->GetXaxis()->SetTitleOffset(1.5);
- Track_XY_Init->GetXaxis()->SetLabelSize(0.05);
- Track_XY_Init->GetYaxis()->SetTitleSize(20);
- Track_XY_Init->GetYaxis()->SetTitleFont(43);
- Track_XY_Init->GetYaxis()->SetLabelSize(0.05);   
- Track_XY_Init->Draw();  
- Track_XY_Fin->Draw("SAME");   
- gPad->BuildLegend(0.2,0.7,0.4,0.9);    
- Track_XY_Init->SetTitle("Track Hits in X-Y Plane");
- c1->SaveAs("Track_XY.pdf");
- c1->SaveAs("Track_XY.png");   
-  
- TLine * line_xy[x_init.size()];
+ Track_XY->GetXaxis()->CenterTitle(true);
+ Track_XY->GetXaxis()->SetTitleSize(20);
+ Track_XY->GetXaxis()->SetTitleFont(43);
+ Track_XY->GetXaxis()->SetTitleOffset(1.5);
+ Track_XY->GetXaxis()->SetLabelSize(0.05);
+ Track_XY->GetYaxis()->SetTitleSize(20);
+ Track_XY->GetYaxis()->SetTitleFont(43);
+ Track_XY->GetYaxis()->SetLabelSize(0.05);   
+ Track_XY->Draw();  
+
+TLine * line_xy[x_init.size()];
  
    for (int n=0; n < x_init.size(); n++){
     line_xy[n] = new TLine(x_init[n],y_init[n],x_fin[n],y_fin[n]);
+    line_xy[n]->SetLineColor(kRed);
     line_xy[n]->Draw("SAME");
    }
- c1->SaveAs("Track_XY_Line.pdf");
- c1->SaveAs("Track_XY_Line.png");    
  
- Track_XZ_Init->GetXaxis()->CenterTitle(true);
- Track_XZ_Init->GetXaxis()->SetTitleSize(20);
- Track_XZ_Init->GetXaxis()->SetTitleFont(43);
- Track_XZ_Init->GetXaxis()->SetTitleOffset(1.5);
- Track_XZ_Init->GetXaxis()->SetLabelSize(0.05);
- Track_XZ_Init->GetYaxis()->SetTitleSize(20);
- Track_XZ_Init->GetYaxis()->SetTitleFont(43);
- Track_XZ_Init->GetYaxis()->SetLabelSize(0.05);   
- Track_XZ_Init->Draw();  
- Track_XZ_Fin->Draw("SAME");  
- gPad->BuildLegend(0.2,0.7,0.4,0.9);    
- Track_XZ_Init->SetTitle("Track Hits in X-Z Plane");
+ c1->SaveAs("Track_XY.pdf");
+ c1->SaveAs("Track_XY.png");   
+ 
+ Track_XZ->GetXaxis()->CenterTitle(true);
+ Track_XZ->GetXaxis()->SetTitleSize(20);
+ Track_XZ->GetXaxis()->SetTitleFont(43);
+ Track_XZ->GetXaxis()->SetTitleOffset(1.5);
+ Track_XZ->GetXaxis()->SetLabelSize(0.05);
+ Track_XZ->GetYaxis()->SetTitleSize(20);
+ Track_XZ->GetYaxis()->SetTitleFont(43);
+ Track_XZ->GetYaxis()->SetLabelSize(0.05);   
+ Track_XZ->Draw();  
+
+TLine * line_xz[x_init.size()];
+ 
+   for (int n=0; n < x_init.size(); n++){
+    line_xz[n] = new TLine(x_init[n],z_init[n],x_fin[n],z_fin[n]);
+    line_xz[n]->SetLineColor(kRed);
+    line_xz[n]->Draw("SAME");
+   }
+ 
  c1->SaveAs("Track_XZ.pdf");
- c1->SaveAs("Track_XZ.png");      
-   
- Track_YZ_Init->GetXaxis()->CenterTitle(true);
- Track_YZ_Init->GetXaxis()->SetTitleSize(20);
- Track_YZ_Init->GetXaxis()->SetTitleFont(43);
- Track_YZ_Init->GetXaxis()->SetTitleOffset(1.5);
- Track_YZ_Init->GetXaxis()->SetLabelSize(0.05);
- Track_YZ_Init->GetYaxis()->SetTitleSize(20);
- Track_YZ_Init->GetYaxis()->SetTitleFont(43);
- Track_YZ_Init->GetYaxis()->SetLabelSize(0.05);   
- Track_YZ_Init->Draw();  
- Track_YZ_Fin->Draw("SAME"); 
- gPad->BuildLegend(0.2,0.7,0.4,0.9);     
- Track_YZ_Init->SetTitle("Track Hits in Y-Z Plane");
+ c1->SaveAs("Track_XZ.png");   
+ 
+ Track_YZ->GetXaxis()->CenterTitle(true);
+ Track_YZ->GetXaxis()->SetTitleSize(20);
+ Track_YZ->GetXaxis()->SetTitleFont(43);
+ Track_YZ->GetXaxis()->SetTitleOffset(1.5);
+ Track_YZ->GetXaxis()->SetLabelSize(0.05);
+ Track_YZ->GetYaxis()->SetTitleSize(20);
+ Track_YZ->GetYaxis()->SetTitleFont(43);
+ Track_YZ->GetYaxis()->SetLabelSize(0.05);   
+ Track_YZ->Draw();  
+
+TLine * line_yz[x_init.size()];
+ 
+   for (int n=0; n < x_init.size(); n++){
+    line_yz[n] = new TLine(y_init[n],z_init[n],y_fin[n],z_fin[n]);
+    line_yz[n]->SetLineColor(kRed);
+    line_yz[n]->Draw("SAME");
+   }
+ 
  c1->SaveAs("Track_YZ.pdf");
- c1->SaveAs("Track_YZ.png");       
-   
+ c1->SaveAs("Track_YZ.png");   
+ 
  Track_XYZ_Init->GetXaxis()->CenterTitle(true);  
  Track_XYZ_Init->GetYaxis()->CenterTitle(true); 
  Track_XYZ_Init->GetXaxis()->SetTitleOffset(2);
  Track_XYZ_Init->GetYaxis()->SetTitleOffset(2);  
  Track_XYZ_Init->GetZaxis()->SetTitleOffset(1.5);     
  Track_XYZ_Init->Draw();  
- Track_XYZ_Fin->Draw("SAME");  
- gPad->BuildLegend(0.2,0.7,0.4,0.9);     
- Track_YZ_Init->SetTitle("Track Hits in 3D");
+
+ TPolyLine3D * line_xyz[x_init.size()];
+ 
+    for (int n=0; n < x_init.size(); n++){
+    line_xyz[n] = new TPolyLine3D(2, {x_init[n], x_fin[n]}, {y_init[n], y_fin[n]}, {z_init[n], z_fin[n]});
+    line_xyz[n]->SetLineColor(kRed);
+    line_xyz[n]->Draw("SAME");
+   }
+ 
  c1->SaveAs("Track_XYZ.pdf");
  c1->SaveAs("Track_XYZ.png");      
 }
