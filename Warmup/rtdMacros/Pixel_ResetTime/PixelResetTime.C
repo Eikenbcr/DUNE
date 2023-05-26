b42a0756a8ca0e5fd007d7fc93d5b3830a1ff6ee
Carter Eikenbary <eikenbcr@tamu.edu>
2023-05-26 18:18:08 -0500

    Update PixelResetTime.C

--- Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
+++ Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
@@ -61,12 +61,10 @@ Bool_t PixelResetTime::Process(Long64_t entry)
        (*event == 0.)
        );  
 
-double tconv_pix1[pixel_reset[598].size()];   
-   
    for (int i=0; i < pixel_reset[598].size(); i++){ 
     if (Event){
-      tconv_pix1[i] = (pixel_reset[598][i]) * 1e+6; 
-      Pixel_Reset_1->Fill(tconv_pix1[i]);           
+      double tconv_pix1 = (pixel_reset[598][i]) * 1e+6; 
+      Pixel_Reset_1->Fill(tconv_pix1);           
     }
    }
    
