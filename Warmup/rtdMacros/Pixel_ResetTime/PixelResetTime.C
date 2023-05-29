936f2cc5169b365023d2b31ffe5cdb6c84d58b6b
Carter Eikenbary <eikenbcr@tamu.edu>
2023-05-29 16:55:13 -0500

    Update PixelResetTime.C

--- Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
+++ Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
@@ -60,6 +60,10 @@ Bool_t PixelResetTime::Process(Long64_t entry)
          bool Event = (
        (*event == 0.)
        );  
+
+if (Event){
+std::cout << "number of pixels in Event 1: " << pixel_x.GetSize() << '\n';   
+}
    
       for (int i=0; i < pixel_x.GetSize(); i++){
          for (int j=0; j <pixel_reset[i].size(); j++){
@@ -89,7 +93,6 @@ Bool_t PixelResetTime::Process(Long64_t entry)
       );       
             
             if (Event && X_Val && Y_Val_1){
-                std::cout << "pixel reset size: " << pixel_reset[i].size() << '\n';
                 double tconv_pix1 = (pixel_reset[i][j]) * 1e+6; 
                 Pixel_Reset_1->Fill(tconv_pix1);           
             }    
