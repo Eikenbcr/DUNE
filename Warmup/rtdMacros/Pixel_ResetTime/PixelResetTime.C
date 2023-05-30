eb91bb9416e8ffdc46f1adc0f89ab12eea207fc6
Carter Eikenbary <eikenbcr@tamu.edu>
2023-05-30 11:43:03 -0500

    Update PixelResetTime.C

--- Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
+++ Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
@@ -28,23 +28,23 @@ void PixelResetTime::SlaveBegin(TTree * /*tree*/)
 {
    TString option = GetOption();
 
-Pixel_Reset_1 = new TH1D("qpixrtd events", "Pixel 1 Reset Frequency", 18000, 200, 2000);
+Pixel_Reset_1 = new TH1D("qpixrtd events", "Pixel [300,7] Reset Frequency", 18000, 200, 2000);
 Pixel_Reset_1->GetXaxis()->SetTitle("time (#mus)");
 Pixel_Reset_1->GetYaxis()->SetTitle("Resets / (0.1 #mus)");
    
-Pixel_Reset_2 = new TH1D("qpixrtd events", "Pixel 2 Reset Frequency", 18000, 200, 2000);
+Pixel_Reset_2 = new TH1D("qpixrtd events", "Pixel [300,10] Reset Frequency", 18000, 200, 2000);
 Pixel_Reset_2->GetXaxis()->SetTitle("time (#mus)");
 Pixel_Reset_2->GetYaxis()->SetTitle("Resets / (0.1 #mus)");   
    
-Pixel_Reset_3 = new TH1D("qpixrtd events", "Pixel 3 Reset Frequency", 18000, 200, 2000);
+Pixel_Reset_3 = new TH1D("qpixrtd events", "Pixel [300,50] Reset Frequency", 18000, 200, 2000);
 Pixel_Reset_3->GetXaxis()->SetTitle("time (#mus)");
 Pixel_Reset_3->GetYaxis()->SetTitle("Resets / (0.1 #mus)");      
    
-Pixel_Reset_4 = new TH1D("qpixrtd events", "Pixel 4 Reset Frequency", 18000, 200, 2000);
+Pixel_Reset_4 = new TH1D("qpixrtd events", "Pixel [300,100] Reset Frequency", 18000, 200, 2000);
 Pixel_Reset_4->GetXaxis()->SetTitle("time (#mus)");
 Pixel_Reset_4->GetYaxis()->SetTitle("Resets / (0.1 #mus)");    
    
-Pixel_Reset_5 = new TH1D("qpixrtd events", "Pixel 5 Reset Frequency", 18000, 200, 2000);
+Pixel_Reset_5 = new TH1D("qpixrtd events", "Pixel [300,500] Reset Frequency", 18000, 200, 2000);
 Pixel_Reset_5->GetXaxis()->SetTitle("time (#mus)");
 Pixel_Reset_5->GetYaxis()->SetTitle("Resets / (0.1 #mus)");     
    
