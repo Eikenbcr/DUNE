279136f410bf14e54962849340e1fca518f4ec78
Carter Eikenbary <eikenbcr@tamu.edu>
2023-05-30 14:57:16 -0500

    Update GaussianFit.C

--- Warmup/rtdMacros/Pixel_ResetTime/GaussianFit.C
+++ Warmup/rtdMacros/Pixel_ResetTime/GaussianFit.C
@@ -9,10 +9,10 @@ Double_t Gaussian_fit(Double_t *v, Double_t *par) {
 //  create a fitting function for a Gaussian signal
 //Par[1]=MeanValue, Par[2]=StandardDeviation    
     Double_t arg = 0;
-    if (par[2] != 0)
-        arg = (v[0] - par[1])/par[2];
+    if (par[1] != 0)
+        arg = (v[0] - par[0])/par[1];
 
-    //  create a Gaussian with width par[2] and mean par[1]
+    //  create a Gaussian with width par[1] and mean par[0]
     Double_t fitval = TMath::Exp(-0.5*arg*arg);
 
     return fitval;
