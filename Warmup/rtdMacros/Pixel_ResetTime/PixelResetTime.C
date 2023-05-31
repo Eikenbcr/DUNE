740b53bc785f7aeedf4e64e2fd88acc6015672fa
Carter Eikenbary <eikenbcr@tamu.edu>
2023-05-31 13:40:33 -0500

    Update PixelResetTime.C

--- Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
+++ Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
@@ -30,8 +30,6 @@ void PixelResetTime::Begin(TTree * /*tree*/)
 void PixelResetTime::SlaveBegin(TTree * /*tree*/)
 {
    TString option = GetOption();
-
-std::cout << "number of active pixels: " << num << '\n'; 
  
 Pixel_Reset_1 = new TH1D("qpixrtd events", "Pixel [300,7] Reset Frequency", 18000, 200, 2000);
 Pixel_Reset_1->GetXaxis()->SetTitle("time (#mus)");
@@ -121,6 +119,7 @@ std::cout << "number of pixels in Event 1: " << pixel_x.GetSize() << '\n';
                 Pixel_Reset_5->Fill(tconv_pix5);           
             }  
 
+TH1D * pix_res[i];          
 str.Form("%02d",i+1);          
 pix_res[i] = new TH1D("qpixrtd events", "Pixel ["+str+"]Reset Frequency", 18000, 200, 2000);
 pix_res[i]->GetXaxis()->SetTitle("time (#mus)");
