68f0637dec8727e3d02e6d884640cabbdca567db
Carter Eikenbary <eikenbcr@tamu.edu>
2023-05-31 14:17:48 -0500

    Update PixelResetTime.C

--- Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
+++ Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
@@ -18,11 +18,12 @@ TH1D * Pixel_Reset_3 = nullptr;
 TH1D * Pixel_Reset_4 = nullptr;
 TH1D * Pixel_Reset_5 = nullptr;
 
+Int_t num = nullptr;
 TFile *f = new TFile("/scratch/user/eikenbcr/DUNE/Warmup/muon_rtd.root");
 TTreeReader reader("event_tree", f);
 TTreeReaderArray<double_t> pix_x(reader, "pixel_x");
  while (reader.Next()) {
-Int_t num = pix_x.GetSize();  
+ num = pix_x.GetSize();  
  }
 TH1D * pix_res[1];   
 double tconv_pix_[1];           
