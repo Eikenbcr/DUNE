bf29b1e40947fac933754263fd162d97d40f1d24
Carter Eikenbary <eikenbcr@tamu.edu>
2023-06-01 13:07:25 -0500

    Update PixelResetTime.C

--- Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
+++ Warmup/rtdMacros/Pixel_ResetTime/PixelResetTime.C
@@ -135,8 +135,12 @@ std::cout << "number of pixels in Event 1: " << pixel_x.GetSize() << '\n';
             
 tconv_pix_[i] = (pixel_reset[i][j]) * 1e+6; 
 pix_res[i]->Fill(tconv_pix_[i]);
-            
-      }
+
+            if (pixel_reset[i].size() > 2.){
+               std::cout << "number of pixels in Event 1 with 3+ resets: " << pixel_x.GetSize() << '\n';  
+            }                
+    
+         }
          }
 }
             
